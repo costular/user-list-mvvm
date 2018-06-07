fdfe4cdf2c79117231ca616ace3632b18ed509c6
Diego Francisco Concepción <diegooo.fc@gmail.com>
2018-06-07 22:54:56 +0200

    Add model Add user layout Add more dependencies

---
+++ app/src/main/java/io/keepcoding/userlist/data/model/UserEntity.kt
@@ -0,0 +1,9 @@
+package io.keepcoding.userlist.data.model
+
+/**
+ * Created by costular on 07/06/2018.
+ */
+data class UserEntity(
+        val name: String,
+        val avatarUrl: String
+)
---
+++ app/src/main/java/io/keepcoding/userlist/data/repository/datasource/FakeDataSource.kt
@@ -0,0 +1,7 @@
+package io.keepcoding.userlist.data.repository.datasource
+
+/**
+ * Created by costular on 07/06/2018.
+ */
+class FakeDataSource {
+}
